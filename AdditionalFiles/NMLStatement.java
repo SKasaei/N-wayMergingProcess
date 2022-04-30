df405bc2873a48dc02a2989f16c59d02200d9662
SKasaei <62388022+SKasaei@users.noreply.github.com>
2022-04-30 19:55:00 +0430

    Add files via upload
    plugins\org.eclipse.epsilon.eol.engine\src\org\eclipse\epsilon\eol\dom\NMLStatement.java

---
+++ AdditionalFiles/NMLStatement.java
@@ -0,0 +1,28 @@
+/*********************************************************************
+* Copyright (c) 2008 The University of York.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+package org.eclipse.epsilon.eol.dom;
+
+import org.eclipse.epsilon.common.module.ModuleElement;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+import org.eclipse.epsilon.eol.execute.context.IEolContext;
+
+public interface NMLStatement {
+	
+	Expression getTargetExpression();
+	void setTargetExpression(Expression targetExpression);
+
+	Expression getValueExpression();
+	void setValueExpression(Expression valueExpression);
+	
+	String getName();
+	void setName(String name);
+	
+	 Expression getExpression1();
+}
